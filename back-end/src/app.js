17a1d7872298a4d59defd12f422f337b0219f888
Dionatan23 <alvestavares1533@gmail.com>
2023-03-30 21:05:43 -0300

    Inicio do Front-end, adicionado o cors

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const router = require('./router');
 
 // Isso permite a API trabalhar com JSON 
 app.use(express.json());
-
+app.use(cors()); // Assim é liberato para todos
 app.use(router);
 
 module.exports = app;
